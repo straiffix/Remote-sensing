595e188a42e7d711e3d0b086456b2aa3f7029235
Fractum <24612656+straiffix@users.noreply.github.com>
2019-05-14 17:53:37 +0200

    Working architecture + first map checking

--- features_extraction.py
+++ features_extraction.py
@@ -19,7 +19,7 @@ json_file.close()
 loaded_model = model_from_json(loaded_model_json)  
 loaded_model.load_weights("model.h5")
 
-img_path = 'dataset/patterns/houses_1.png'
+img_path = 'dataset/patterns/trees_4.png'
 img = image.load_img(img_path, target_size=(120, 120))
 img_tensor = image.img_to_array(img)
 img_tensor = np.expand_dims(img_tensor, axis=0)
@@ -37,30 +37,30 @@ layer_names = []
 for layer in loaded_model.layers[:16]:
     layer_names.append(layer.name) # Names of the layers, so you can have them as part of your plot
 
-#images_per_row = 16
-#for layer_name, layer_activation in zip(layer_names, activations): # Displays the feature maps
-#    n_features = layer_activation.shape[-1] # Number of features in the feature map
-#    size = layer_activation.shape[1] #The feature map has shape (1, size, size, n_features).
-#    n_cols = n_features // images_per_row # Tiles the activation channels in this matrix
-#    display_grid = np.zeros((size * n_cols, images_per_row * size))
-#    for col in range(n_cols): # Tiles each filter into a big horizontal grid
-#        for row in range(images_per_row):
-#            channel_image = layer_activation[0,
-#                                             :, :,
-#                                             col * images_per_row + row]
-#            channel_image -= channel_image.mean() # Post-processes the feature to make it visually palatable
-#            channel_image /= channel_image.std()
-#            channel_image *= 64
-#            channel_image += 128
-#            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
-#            display_grid[col * size : (col + 1) * size, # Displays the grid
-#                         row * size : (row + 1) * size] = channel_image
-#    scale = 1. / size
-#    plt.figure(figsize=(scale * display_grid.shape[1],
-#                        scale * display_grid.shape[0]))
-#    plt.title(layer_name)
-#    plt.grid(False)
-#    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+images_per_row = 16
+for layer_name, layer_activation in zip(layer_names, activations): # Displays the feature maps
+    n_features = layer_activation.shape[-1] # Number of features in the feature map
+    size = layer_activation.shape[1] #The feature map has shape (1, size, size, n_features).
+    n_cols = n_features // images_per_row # Tiles the activation channels in this matrix
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+    for col in range(n_cols): # Tiles each filter into a big horizontal grid
+        for row in range(images_per_row):
+            channel_image = layer_activation[0,
+                                             :, :,
+                                             col * images_per_row + row]
+            channel_image -= channel_image.mean() # Post-processes the feature to make it visually palatable
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size : (col + 1) * size, # Displays the grid
+                         row * size : (row + 1) * size] = channel_image
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1],
+                        scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
     
     lactiv = activations[4]
 channel_image = lactiv[0, :, :, 5]
--- model.py
+++ model.py
@@ -5,40 +5,71 @@ Created on Fri Apr 19 15:18:26 2019
 @author: strai
 """
 
-from keras import layers
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
-from functools import partial
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.optimizers import rmsprop
 
 
-model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding='same', input_shape = (112, 112, 3), activation = 'relu'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5)) # antes era 0.25
+n_classes = 3
+#model = Sequential()
 
-# Adding a second convolutional layer
-model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5)) # antes era 0.25
-
-# Adding a third convolutional layer
-model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5)) # antes era 0.25
-
-# Step 3 - Flattening
-model.add(Flatten())
-
-# Step 4 - Full connection
-model.add(Dense(units = 512, activation = 'relu'))
-model.add(Dropout(0.5)) 
-model.add(Dense(units = 3, activation = 'relu'))
+# Layer 1
+#
+#model.add(Conv2D(96, (11, 11), input_shape=(224, 224, 3), 
+#                 padding='same', kernel_regularizer=l2(0.)))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+#
+## Layer 2
+#model.add(Conv2D(256, (5, 5), padding='same'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+#	# Layer 3
+#model.add(ZeroPadding2D((1, 1)))
+#model.add(Conv2D(512, (3, 3), padding='same'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+#	# Layer 4
+#model.add(ZeroPadding2D((1, 1)))
+#model.add(Conv2D(1024, (3, 3), padding='same'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#
+#	# Layer 5
+#model.add(ZeroPadding2D((1, 1)))
+#model.add(Conv2D(1024, (3, 3), padding='same'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+#	# Layer 6
+#model.add(Flatten())
+#model.add(Dense(3072))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+#
+#	# Layer 7
+#model.add(Dense(4096))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+#
+#	# Layer 8
+#model.add(Dense(n_classes))
+#model.add(BatchNormalization())
+#model.add(Activation('softmax'))
 
 #model = Sequential()
 #model.add(Conv2D(32, (3, 3), input_shape=(120, 120, 3)))
@@ -58,14 +89,27 @@ model.add(Dense(units = 3, activation = 'relu'))
 #model.add(Activation('relu'))
 #
 #model.add(Dropout(0.5))
-#model.add(Dense(1))
-#model.add(Activation('sigmoid'))
-#
-model.compile(optimizer = 'rmsprop',
+#model.add(Dense(3))
+#model.add(Activation('softmax'))
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(120, 120, 3)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3, activation='softmax'))
+
+model.compile(optimizer = rmsprop(lr=0.0001, decay=1e-6),
                    loss = 'categorical_crossentropy', 
                    metrics = ['accuracy'])
 
 batch_size = 16
+#batch_size = 128
 
 train_datagen = ImageDataGenerator(
         rescale = 1./255,
@@ -77,22 +121,24 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
         'dataset/train',
-        target_size=(112, 112),
+        target_size=(120, 120),
         batch_size = 16,
         class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
         'dataset/validation',
-        target_size=(112, 112),
+        target_size=(120, 120),
         batch_size=16,
         class_mode = 'categorical')
-
+#
 model.fit_generator(
         train_generator,
-        steps_per_epoch=2000 // 16,
-        epochs = 50,
+        steps_per_epoch=701 // 16,
+        epochs = 15,
         validation_data=validation_generator,
-        validation_steps= 800 // 16)
+        validation_steps= 79 // 16
+        )
+#validation_steps= 800 // 128
 
 model_json=model.to_json()
 with open("model.json", "w") as json_file:
